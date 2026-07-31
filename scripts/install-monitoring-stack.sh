20c688462b79f475c1ef838a448d1c27a7baf90b
deekshi-1 <deekshithc1@gmail.com>
2026-07-31 23:26:36 +0530

    nodeport

--- scripts/install-monitoring-stack.sh
+++ scripts/install-monitoring-stack.sh
@@ -12,27 +12,28 @@ https://grafana.github.io/helm-charts
 
 helm repo update
 
-
 echo "Creating monitoring namespace..."
 
 kubectl create namespace monitoring \
 --dry-run=client -o yaml | kubectl apply -f -
 
-
-echo "Deploying Prometheus..."
+echo "Deploying Prometheus Stack..."
 
 helm upgrade --install kube-prometheus-stack \
 prometheus-community/kube-prometheus-stack \
 -n monitoring \
--f monitoring/prometheus/prometheus-values.yaml
-
+-f monitoring/prometheus/prometheus-values.yaml \
+--set grafana.enabled=false \
+--set prometheus.service.type=NodePort \
+--set prometheus.service.nodePort=32090
 
 echo "Deploying Grafana..."
 
 helm upgrade --install grafana \
 grafana/grafana \
 --namespace monitoring \
--f monitoring/grafana/grafana-values.yaml
-
+-f monitoring/grafana/grafana-values.yaml \
+--set service.type=NodePort \
+--set service.nodePort=32000
 
 echo "Monitoring stack deployed successfully!"
